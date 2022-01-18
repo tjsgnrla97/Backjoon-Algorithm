967e2ff16c65e82dae336a563dbb0533e9a244e2
김 선 후 <tjsgnrla97@gmail.com>
2022-01-18 16:11:16 +0900

    backjoon java

---
+++ Backjoon_java/src/step01/Java_1000.java
@@ -0,0 +1,13 @@
+package step01;
+import java.util.Scanner;
+public class Java_1000 {
+	public static void main(String[] args) {
+		// 5.A+B
+		Scanner sc = new Scanner(System.in);
+		int a,b,sum;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sum = a + b;
+		System.out.print(sum);
+	}
+}
---
+++ Backjoon_java/src/step01/Java_1001.java
@@ -0,0 +1,27 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Java_1001 {
+	public static void main(String[] args) {
+		// 6.A-B
+		Scanner sc = new Scanner(System.in);
+		int a, b, sum;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sum = a - b;
+		System.out.print(sum);
+		/*
+		 * public static void main(String[] args) throws IOException { BufferedReader
+		 * br= new BufferedReader(new InputStreamReader(System.in));
+		 * 
+		 * String str = br.readLine(); StringTokenizer st = new StringTokenizer(str,
+		 * " ");
+		 * 
+		 * int A = Integer.parseInt(st.nextToken()); int B =
+		 * Integer.parseInt(st.nextToken());
+		 * 
+		 * System.out.println(A - B);
+		 */
+	}
+}
