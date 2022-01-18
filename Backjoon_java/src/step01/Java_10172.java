24591cfa4ddcc6c9c99bbacdfcac0112d2047b7e
김 선 후 <tjsgnrla97@gmail.com>
2022-01-18 15:27:27 +0900

    backjoon java
    스탭1 문제 풀이

---
+++ Backjoon_java/src/step01/Java_10172.java
@@ -0,0 +1,11 @@
+package step01;
+public class Java_10172 {
+	public static void main(String[] args) {
+		//4.개
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.print("||_/=\\\\__|");
+	}
+}
---
+++ Backjoon_java/src/step01/Java_10718.java
@@ -0,0 +1,9 @@
+package step01;
+
+public class Java_10718 {
+	public static void main(String[] args) {
+		//2.We love kriii
+		System.out.println("강한친구 대한육군");
+		System.out.print("강한친구 대한육군");
+	}
+}
