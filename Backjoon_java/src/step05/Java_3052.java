c31b635dc9c56bbfd8862a402cde040de5ccec8f
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-02-16 12:36:40 +0900

    Backjoon java

---
+++ Backjoon_java/src/step05/Java_3052.java
@@ -0,0 +1,30 @@
+package step05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_3052 {
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer stk;
+	static StringBuilder sb= new StringBuilder();
+	static boolean[] arr=new boolean[42];
+	static int cnt=0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		//4.나머지
+		for(int i=0; i<10; i++) {
+			arr[Integer.parseInt(in.readLine())%42] = true;
+		}
+		for(int i=0; i<42; i++) {
+			if(arr[i]) cnt++;
+		}
+		out.write(cnt+"");
+		in.close();
+		out.flush();
+		out.close();
+	}
+}
