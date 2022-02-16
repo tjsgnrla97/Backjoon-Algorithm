7f67ae39e055cb1f7fd6cfdd30f1d942c1fd3915
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-02-16 12:39:30 +0900

    Backjoon java

--- Backjoon_java/src/step05/Java_3052.java
+++ Backjoon_java/src/step05/Java_3052.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Java_3052 {
@@ -16,13 +17,16 @@ public class Java_3052 {
 	static int cnt=0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//4.나머지
-		for(int i=0; i<10; i++) {
-			arr[Integer.parseInt(in.readLine())%42] = true;
-		}
-		for(int i=0; i<42; i++) {
-			if(arr[i]) cnt++;
-		}
-		out.write(cnt+"");
+//		for(int i=0; i<10; i++) {
+//			arr[Integer.parseInt(in.readLine())%42] = true;
+//		}
+//		for(int i=0; i<42; i++) {
+//			if(arr[i]) cnt++;
+//		}
+		HashSet<Integer> h = new HashSet<Integer>();
+		for(int i=0; i<10; i++) h.add(Integer.parseInt(in.readLine())%42);
+//		out.write(cnt+"");
+		out.write(h.size()+"");
 		in.close();
 		out.flush();
 		out.close();
