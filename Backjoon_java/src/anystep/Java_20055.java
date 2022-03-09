a8de80e063739328ed99c0f08936c76847c8bc23
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-03-09 09:57:39 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_20055.java
@@ -0,0 +1,112 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_20055 {
+	// 컨테이어 벨트 위의 로봇
+//	문제 지문이 굉장히 불친절하고 난해해서 구현목표자체가 이해하기가 어려웠습니다.
+//	특히 로봇이 대체 어느시점, 어느단계에서 벨트에 올라가는지, 벨트에서 내려와서 어찌 행동하는지를
+//	이해하기까지 시간을 많이 사용한 문제였습니다.
+//	차근차근 단계별로 동작을 요약해보자면 다음과 같다.
+//	1. 벨트 한칸 회전
+//	2. 로봇 이동(앞이 빈공간이고 내구도 1이상)
+//	3. 로봇 올리기 - 여기서 로봇이 벨트에 올라감.!!
+//	4. 내구도 체크 (내구도 0 K개 이상일시 단계종료) 후에 1단계로 순환.
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer stk;
+	static int N, K, ans;
+	static Belt[] beltPoint;
+	static int SIZE;
+	static class Belt{
+		int durability;
+		boolean robot;
+		public Belt(int durability, boolean robot) {
+			super();
+			this.durability = durability;
+			this.robot = robot;
+		}
+		
+	}
+	public static void main(String[] args) throws IOException {
+		stk = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(stk.nextToken());
+		K = Integer.parseInt(stk.nextToken());
+		SIZE = 2*N;
+		beltPoint = new Belt[SIZE];
+		
+		stk = new StringTokenizer(in.readLine());
+		for(int i=0; i<SIZE; i++) beltPoint[i]=new Belt(Integer.parseInt(stk.nextToken()), false);
+		ans = startProcess();
+		out.write(ans+"");
+		out.flush();
+		out.close();
+		in.close();
+	}
+	private static int startProcess() {
+		int step=0;
+		while(true) {
+			step++; //단계횟수증가
+			rotateBelt(); //1.벨트 한칸 회전
+			moveRobot();  //2.로봇 이동
+			upRobot();	  //3.로봇 올리기 - 로봇추가
+			if(checkDurability()) break; //4. 내구도 체크
+		}
+		return step;
+	}
+	private static boolean checkDurability() {
+		int cnt=0;
+		for(int i=0; i<SIZE; i++) {
+			if(beltPoint[i].durability==0) cnt++;
+		}
+		return cnt>=K ? true : false;
+	}
+	private static void upRobot() {
+		//맨 처음 위치에 로봇이 없고 내구도 1이상일때
+		if(!beltPoint[0].robot && beltPoint[0].durability>=1) {
+			beltPoint[0].durability--;
+			beltPoint[0].robot = true;
+		}
+	}
+	private static void moveRobot() {
+		int halfSize = SIZE / 2;
+		for(int i = halfSize-2; i>=0; i--) {
+			if(beltPoint[i].robot) { //해당 인덱스에 로봇이 있다면
+				if(i+1 == halfSize-1) { //다음 인덱스가 절반이라면
+					if(beltPoint[i+1].durability>=1) { //다음 인덱스 내구도가 1이상이면
+						beltPoint[i].robot = false;    //로봇을 바로 내린다.
+						beltPoint[i+1].durability--;   //다음 인덱스의 내구도를 내린다.
+						continue;
+					}
+				}
+				//다음 인덱스에 이미 로봇이 있거나 내구도가 0인경우 스킵
+				if(beltPoint[i+1].robot || beltPoint[i+1].durability==0) continue;
+				beltPoint[i+1].robot = true;
+				beltPoint[i+1].durability--;
+				beltPoint[i].robot=false;
+			}
+		}
+		
+	}
+	private static void rotateBelt() {
+		//마지막 인덱스 배열값 임시저장
+		boolean tempRobot = beltPoint[SIZE-1].robot;
+		int tempDurability = beltPoint[SIZE-1].durability;
+		int halfSize = SIZE / 2;
+		for(int i = SIZE-1; i>=1; i--) {
+			beltPoint[i].robot = beltPoint[i-1].robot;
+			beltPoint[i].durability = beltPoint[i-1].durability;
+			if(i==halfSize-1) beltPoint[i].robot = false; // 절반구역에서 로봇은 바로 내려간다.
+		}
+		//덮어쒸어지기 전 마지막 인덱스 배열값 첫번째 값으로 넣기
+		beltPoint[0].robot = tempRobot;
+		beltPoint[0].durability = tempDurability;
+	}
+
+}
--- Backjoon_java/src/step06/Java_1065.java
+++ Backjoon_java/src/step06/Java_1065.java
@@ -1,10 +1,41 @@
 package step06;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Java_1065 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// 3.한수
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer stk;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int num = Integer.parseInt(in.readLine());
+		int ans = arithmetic_sequence(num);
+		out.append(Integer.toString(ans));
+		out.flush();
+		out.close();
+		in.close();
+	}
+	private static int arithmetic_sequence(int num) {
+		int ans = 0;
+		if(num<100) return num;
+		else {
+			ans=99;
+			if(num==1000) num=999;
+			for(int i=100; i<=num; i++) {
+				int hun = i / 100;
+				int ten = (i/10)%10;
+				int one = i%10;
+				
+				if((hun-ten)==(ten-one)) ans++;
+			}
+		}
+		return ans;
 	}
 
 }
