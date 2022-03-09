e5d2f1f6e127c29eb0ac805da59769110f854485
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-03-09 10:32:19 +0900

    Backjoon java

--- Backjoon_java/src/anystep/Java_8983.java
+++ Backjoon_java/src/anystep/Java_8983.java
@@ -10,16 +10,16 @@ import java.util.StringTokenizer;
 
 public class Java_8983 {
 	//사냥꾼
-	//처음에는 예전에 실습시간에 풀어보았던 충전기 문제와 체스판 문제와 유사하여
+//	처음에는 예전에 실습시간에 풀어보았던 충전기 문제와 체스판 문제와 유사하여
 //	브루트포스를 가장 먼저 떠올리고 모든 동물과 사대를 탐색하며 풀어본 결과 TLE가 떠서 실패.
-//	O(M*N)의 해결방법이 아닌 다른 방법이 필요.
+//	O(M*N)의 해결방법이 아닌 다른 방법이 필요하다는 걸 알았습니다.
 //	그러다가 결국 동물의 y좌표가 L을 넘어가는 경우는 처음부터 고려할 필요자체가 없다는 것을 깨닫고
-//	동물을 기준으로 x좌표를 기준으로 반복문을 생각해봄.
-//	동물이 모든 사대 중 자신을 잡을 수 있는 사대 한개만 찾으면 되는 문제다.
+//	동물을 기준으로 x좌표를 기준으로 반복문을 생각해봤습니다.
+//	동물이 모든 사대 중 자신을 잡을 수 있는 사대 한개만 찾으면 되는 문제.
 //	가장 확실하게 범위내에 포함되는 사대는 사대의 x좌표와 동물의 x좌표가 같은경우(단 y좌표가 L이하)
 //	따라서 동물x좌표에서 가장 가까운 x좌표를 지니는 사대를 찾으면 된다.(단 사정거리 L을 만족하는 범위)
-//	x좌표에 따라서 오름차순으로 위치가 정렬되어야 한다.
-//	동물의 x좌표 기준 왼쪽과 오른쪽을 확인해본다.
+//	x좌표에 따라서 오름차순으로 위치가 정렬되어야 합니다.
+//	동물의 x좌표 기준 왼쪽과 오른쪽을 확인해봅니다.
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
