eec53c7d08e5a8ab0d5f32db934e6d473d3f75ea
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-03-10 17:07:24 +0900

    Backjoon java

---
+++ Backjoon_java/src/step07/Java_11654.java
@@ -0,0 +1,13 @@
+package step07;
+
+import java.io.IOException;
+
+public class Java_11654 {
+
+	public static void main(String[] args) throws IOException{
+		// 1.아스키 코드
+		int ch = System.in.read();
+		System.out.println(ch);
+	}
+
+}
