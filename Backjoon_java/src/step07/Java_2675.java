7101d949368d62d083e359001cb31434fd26b5b2
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-03-17 16:43:45 +0900

    Backjoon java

---
+++ Backjoon_java/src/step07/Java_2675.java
@@ -0,0 +1,22 @@
+package step07;
+
+import java.util.Scanner;
+
+public class Java_2675 {
+	//4. 문자열 반복
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for(int tc=1; tc<=T; tc++) {
+			int repeat = in.nextInt();
+			String str = in.next();
+			for(int i=0; i<str.length(); i++) {
+				for(int j=0; j<repeat; j++) {
+					System.out.print(str.charAt(i));
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
