f2e558445ce3e112ac9147c5d6035b5ef751b995
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-03-29 21:52:39 +0900

    Backjoon java

---
+++ Backjoon_java/src/step08/Java_2292.java
@@ -0,0 +1,25 @@
+package step08;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Java_2292 {
+
+	public static void main(String[] args) throws IOException {
+		//2.
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		solve(A);
+	}
+
+	private static void solve(int a) {
+		int i=1;
+		int n = a-1;
+		while(n>i*6) {
+			n-=i*6;
+			i++;
+		}
+		System.out.println(a != 1 ? i+1 : 1);
+	}
+
+}
