4b1c9adb0835378e3369efdf8f0ca35ed65cfa40
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-05-12 16:33:57 +0900

    Backjoon java

---
+++ Backjoon_java/src/step01/Java_25083.java
@@ -0,0 +1,17 @@
+package step01;
+
+public class Java_25083 {
+	// 새싹
+	public static void main(String[] args) {
+		
+		String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+				   "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+				   " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+				   "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+				   "      |\n" + 			// \n 이 제어문자다.
+				   "      |";
+
+		System.out.print(s);
+	}
+
+}
