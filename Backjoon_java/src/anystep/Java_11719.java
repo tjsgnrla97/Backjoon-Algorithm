2fefa84bd58c9b5398bb99a4ae00c85ad43b183e
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-05-24 17:58:36 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_11719.java
@@ -0,0 +1,22 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_11719 {
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer stk;
+	public static void main(String[] args) throws IOException {
+		String str;
+		while((str=in.readLine())!=null) {
+			System.out.println(str);
+		}
+	}
+
+}
