16e6c6294d92eb76e9ab362150ecb9eff939cc79
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-05-27 13:01:35 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_5337.java
@@ -0,0 +1,11 @@
+package anystep;
+
+public class Java_5337 {
+	// 웰컴
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
