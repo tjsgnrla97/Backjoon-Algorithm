a73360db8f795fcbdf0f189d359b08a26526aae2
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-05-30 17:59:56 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_9653.java
@@ -0,0 +1,29 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_9653 {
+	//스타워즈 로고
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer stk;
+	public static void main(String[] args) {
+		System.out.print("    8888888888  888    88888\n"
+				+ "   88     88   88 88   88  88\n"
+				+ "    8888  88  88   88  88888\n"
+				+ "       88 88 888888888 88   88\n"
+				+ "88888888  88 88     88 88    888888\n"
+				+ "\n"
+				+ "88  88  88   888    88888    888888\n"
+				+ "88  88  88  88 88   88  88  88\n"
+				+ "88 8888 88 88   88  88888    8888\n"
+				+ " 888  888 888888888 88  88      88\n"
+				+ "  88  88  88     88 88   88888888");
+	}
+
+}
