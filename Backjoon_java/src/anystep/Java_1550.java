a3ed8a0d2fffddfe25608e5790bde58885e859bd
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-06-01 16:43:11 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_1550.java
@@ -0,0 +1,22 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_1550 {
+	// 16진수
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer stk;
+	public static void main(String[] args) throws IOException {
+		String str = in.readLine();
+		int ans = Integer.parseInt(str, 16);
+		System.out.println(ans);
+	}
+
+}
