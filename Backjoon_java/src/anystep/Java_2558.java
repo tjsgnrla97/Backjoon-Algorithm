6f66155e5bd4138a17be8832a7ac72b8b167d476
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-06-12 16:14:56 +0900

    Backjoon java

---
+++ Backjoon_java/src/anystep/Java_2558.java
@@ -0,0 +1,23 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_2558 {
+	// A+B - 2
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer stk;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int a = Integer.parseInt(in.readLine());
+		int b = Integer.parseInt(in.readLine());
+		System.out.println(a+b);
+
+	}
+
+}
