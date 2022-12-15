b748c530e00f75d5be50831acbbc9deb7618e41a
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-12-15 16:49:44 +0900

    feat: 🎸 Backjoon Math Java_16394 홍익대학교

---
+++ Backjoon_java/src/anystep/Java_16394.java
@@ -0,0 +1,16 @@
+package anystep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_16394 {
+    //홍익대학교
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(Integer.parseInt(in.readLine()) - 1946);
+    }
+}
