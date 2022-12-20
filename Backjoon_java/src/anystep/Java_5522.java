33950a925ec7e2f5e39949d1775ae2b544f0b983
tjsgnrla97 <tjsgnrla97@gmail.com>
2022-12-20 16:20:34 +0900

    feat: 🎸 Backjoon Math Java_5522 카드 게임

--- Backjoon_java/src/anystep/Java_5522.java
+++ Backjoon_java/src/anystep/Java_5522.java
@@ -3,21 +3,18 @@ package anystep;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Java_5554 {
-    //심부름 가는 길
+public class Java_5522 {
+    //카드 게임
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer stk;
     static StringBuilder sb = new StringBuilder();
+    static int ans;
 
     public static void main(String[] args) throws IOException {
-        int result = 0;
-        for (int i = 0; i < 4; i++) {
-            result += Integer.parseInt(in.readLine());
+        for (int i = 0; i < 5; i++) {
+            ans += Integer.parseInt(in.readLine());
         }
-        int min = result / 60;
-        int sec = result % 60;
-        System.out.println(min);
-        System.out.println(sec);
+        System.out.println(ans);
     }
 }
