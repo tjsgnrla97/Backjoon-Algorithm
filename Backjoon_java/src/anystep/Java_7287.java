0c6738cd9230f2eea39a7a87fd7eaaa8fc58bebd
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-05 09:43:31 +0900

    feat: 🎸 Backjoon Implement Java_7287 등록

---
+++ Backjoon_java/src/anystep/Java_7287.java
@@ -0,0 +1,20 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_7287 {
+    //등록
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println(471);
+        System.out.println("tjsgnrla");
+    }
+}
