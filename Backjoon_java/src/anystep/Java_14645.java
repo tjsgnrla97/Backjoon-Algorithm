11ce91c5eb1e078cc44006fa11f37ad387c32ac2
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-08 10:34:25 +0900

    feat: 🎸 Backjoon Implement Java_14645 와이버스 부릉부릉

---
+++ Backjoon_java/src/anystep/Java_14645.java
@@ -0,0 +1,19 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_14645 {
+    //와이버스 부릉부릉
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("비와이");
+    }
+}
