00acbae83034eae21e913e90ca4fdf1feba99880
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-11 10:08:42 +0900

    feat: 🎸 Backjoon Implement Java_15962 새로운 시작

---
+++ Backjoon_java/src/anystep/Java_15962.java
@@ -0,0 +1,19 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_15962 {
+    //새로운 시작
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("파이팅!!");
+    }
+}
