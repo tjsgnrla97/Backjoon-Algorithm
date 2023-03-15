2fe5b38002d9e203a41492dc29611e7f555c1c9f
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-15 11:17:52 +0900

    feat: 🎸 Backjoon Implement Java_2523 별 찍기 - 13

---
+++ Backjoon_java/src/anystep/Java_2523.java
@@ -0,0 +1,28 @@
+package anystep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_2523 {
+    //별 찍기 - 13
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(in.readLine());
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
