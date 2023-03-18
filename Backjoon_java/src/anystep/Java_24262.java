5ecbdda1ff108bcaff782a45ac937fa84f3ba813
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-18 11:07:52 +0900

    feat: 🎸 Backjoon Implement Java_24262 package anystep;

---
+++ Backjoon_java/src/anystep/Java_24262.java
@@ -0,0 +1,20 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_24262 {
+    //알고리즘 수업 - 알고리즘의 수행 시간 1
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
