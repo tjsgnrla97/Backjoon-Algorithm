21771a380c510316a07b6eb8574c43583187e153
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-20 11:36:59 +0900

    feat: 🎸 Backjoon Implement Java_5339 콜센터

---
+++ Backjoon_java/src/anystep/Java_5339.java
@@ -0,0 +1,28 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_5339 {
+    //콜센터
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
