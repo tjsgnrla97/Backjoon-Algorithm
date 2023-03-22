34328bad901b0adff12985ac4f3451cb77b94b7b
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-22 10:04:57 +0900

    feat: 🎸 Backjoon Implement Java_23234 The World Responds

---
+++ Backjoon_java/src/anystep/Java_23234.java
@@ -0,0 +1,19 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_23234 {
+    //The World Responds
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("The world says hello!");
+    }
+}
