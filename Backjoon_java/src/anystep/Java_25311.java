3aad948e8e1d763f1f94c32a74a391a4ab3ad57d
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-03-23 11:05:29 +0900

    feat: 🎸 Backjoon Implement Java_25311 package anystep;
        //UCPC에서 가장 쉬운 문제 번호는?

---
+++ Backjoon_java/src/anystep/Java_25311.java
@@ -0,0 +1,19 @@
+package anystep;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Java_25311 {
+    //UCPC에서 가장 쉬운 문제 번호는?
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
