7aa18bbb093dcd52189ddda0fcad9a64f8df3de5
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-04-05 10:06:17 +0900

    feat: 🎸 Backjoon Implement Java_1264 모음의 개수

---
+++ Backjoon_java/src/anystep/Java_1264.java
@@ -0,0 +1,34 @@
+package anystep;
+
+import java.util.Scanner;
+
+public class Java_1264 {
+    //모음의 개수
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (; ; ) {
+            String str = sc.nextLine();
+            int count = 0;
+            if (str.equals("#")) {
+                break;
+            }
+
+            str = str.toLowerCase();
+
+            for (int i = 0; i < str.length(); i++) {
+                switch (str.charAt(i)) {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        count++;
+                    default:
+                        break;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
