4b1753c93892218363661399dae585425eac285b
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-04-06 11:07:28 +0900

    feat: 🎸 Backjoon Implement Java_6749 Next in line

---
+++ Backjoon_java/src/anystep/Java_6749.java
@@ -0,0 +1,14 @@
+package anystep;
+
+import java.util.Scanner;
+
+public class Java_6749 {
+    //Next in line
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int differ = b - a;
+        System.out.println(b + differ);
+    }
+}
