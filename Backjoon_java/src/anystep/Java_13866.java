e7e7a04168233cefa0aff1928735d9761e514016
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-04-13 11:39:36 +0900

    feat: 🎸 Backjoon Implement Java_13866 팀 나누기

---
+++ Backjoon_java/src/anystep/Java_13866.java
@@ -0,0 +1,13 @@
+package anystep;
+
+import java.util.Scanner;
+
+public class Java_13866 {
+    //팀 나누기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt();
+        int C = sc.nextInt(), D = sc.nextInt();
+        System.out.println(Math.abs((A + D) - (B + C)));
+    }
+}
