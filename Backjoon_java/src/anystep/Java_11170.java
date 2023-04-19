0177dfc081b6d41f4089c5567906df20193c6713
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-04-19 18:30:24 +0900

    feat: 🎸 Backjoon Implement Java_11170 0의 개수

---
+++ Backjoon_java/src/anystep/Java_11170.java
@@ -0,0 +1,38 @@
+package anystep;
+
+import java.util.Scanner;
+
+public class Java_11170 {
+    //0의 개수
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            int cnt = 0;
+
+            for (int j = N; j <= M; j++) {
+                int t = j;
+                if (t == 0) {
+                    cnt++;
+                    continue;
+                }
+
+                while (t > 1) {
+                    if (t % 10 == 0) {
+                        cnt++;
+                    }
+                    t /= 10;
+                }
+
+            }
+
+            System.out.println(cnt);
+
+        }
+        sc.close();
+    }
+}
