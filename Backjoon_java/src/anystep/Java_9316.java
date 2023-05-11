b01cee75e068f3956d0c0ea03d6293f8fa1d3ea4
tjsgnrla97 <tjsgnrla97@gmail.com>
2023-05-11 16:56:15 +0900

    feat: 🎸 Backjoon Implement Java_9316 Hello Judge

---
+++ Backjoon_java/src/anystep/Java_9316.java
@@ -0,0 +1,19 @@
+package anystep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_9316 {
+    //Hello Judge
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer stk;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(in.readLine());
+        for (int i = 1; i <= N; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+}
