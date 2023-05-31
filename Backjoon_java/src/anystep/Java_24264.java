d03d3577e25d457a66df1c3ac273543f2a205251
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-05-31 12:53:38 +0900

    feat: backjoon Implement Java_24264 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ Backjoon_java/src/anystep/Java_24264.java
@@ -0,0 +1,18 @@
+package anystep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_24264 {
+    //알고리즘 수업 - 알고리즘의 수행 시간 3
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Long N = Long.parseLong(in.readLine());
+        System.out.println(N * N);
+        System.out.println(2);
+    }
+}
