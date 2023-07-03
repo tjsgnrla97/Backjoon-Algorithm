3f8bc289aad1336be121d96635da9a4f741d7869
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-07-04 07:31:25 +0900

    feat: backjoon Implementation Java_9295 주사위

---
+++ Backjoon_java/src/anystep/Java_9295.java
@@ -0,0 +1,20 @@
+package anystep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_9295 {
+    //주사위
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stk;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(in.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            stk = new StringTokenizer(in.readLine());
+            System.out.println(String.format("Case %d: %d", tc, Integer.parseInt(stk.nextToken()) + Integer.parseInt(stk.nextToken())));
+        }
+    }
+}
