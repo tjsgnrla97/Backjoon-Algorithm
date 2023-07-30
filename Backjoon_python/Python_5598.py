84abfd5d1026a04ce449a06bf76040a28e1a2cfe
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-07-30 13:25:06 +0900

    feat: backjoon Math Python_5598 카이사르 암호

--- Backjoon_python/Python_5598.py
+++ Backjoon_python/Python_5598.py
@@ -11,3 +11,13 @@ for m in msg:
                 cnt += 26
             ans += alpha[cnt]
 print(ans)
+
+"""
+li = list(input())
+for i in range(len(li)):
+    k = ord(li[i]) - 3
+    if k < ord('A'):
+        k += 26
+    li[i] = chr(k)
+print(''.join(li))
+"""
