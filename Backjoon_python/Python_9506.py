340382edc831e3ae5bc41c1eabe8f7e32460db2a
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-08-02 12:46:39 +0900

    feat: backjoon Math Python_9506 약수들의 합\

--- Backjoon_python/Python_9506.py
+++ Backjoon_python/Python_9506.py
@@ -19,3 +19,17 @@ while True:
                 print(f"{i}",end=" + ")
     else:
         print(f"{num} is NOT perfect.")
+'''
+while True:
+    n = int(input())
+    if n == -1: # 입력 값이 -1이면 반복문 종료
+        break;
+    arr = []
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(n, " = ", " + ".join(str(i) for i in arr), sep="")
+    else:
+        print(n, "is NOT perfect.")
+'''
