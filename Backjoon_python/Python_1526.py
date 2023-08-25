0b0e06d66eee4e486a6b9cb64e1489e890d0e249
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-08-25 18:26:29 +0900

    feat: backjoon Math Python_1526 가장 큰 금민수

---
+++ Backjoon_python/Python_1526.py
@@ -0,0 +1,12 @@
+#가장 큰 금민수
+n = int(input())
+
+while True:
+    flag = True
+    for i in str(n):
+        if i != "4" and i != "7":
+            flag = False
+            n -= 1
+    if flag:
+        print(n)
+        break 
