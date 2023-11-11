efd1e8fe1c9f2b9948eaa23573cba5d7cb04f77a
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-11-11 17:06:21 +0900

    feat: backjoon BF Python_25494 단순한 문제 (Small)

---
+++ Backjoon_python/Python_25494.py
@@ -0,0 +1,11 @@
+#단순한 문제 (Small)
+T = int(input())
+for tc in range(T):
+    a,b,c = map(int, input().split())
+    ans = 0
+    for x in range(1,a+1):
+        for y in range(1,b+1):
+            for z in range(1,c+1):
+                if x % y == y % z == z % x:
+                    ans += 1
+    print(ans)
