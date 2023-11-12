7ac759de4948eb35d48cb17255355543b7d408b4
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-11-12 14:05:08 +0900

    feat: backjoon BF Python_18312 시각

---
+++ Backjoon_python/Python_18312.py
@@ -0,0 +1,18 @@
+#시각
+n, k = map(int, input().split())
+h,m,s = 0,0,0
+ans = 0
+k = str(k)
+for h in range(0,n+1):
+    if h<10:
+        h = '0'+str(h)
+    for m in range(0,60):
+        if m < 10:
+            m = '0' + str(m)
+        for s in range(0,60):
+            if s < 10:
+                s = '0' + str(s)
+            if k in str(h) or k in str(m) or k in str(s):
+                ans += 1
+
+print(ans)
