b8c18b4971c01dbf9e68eb7a99a65f2e36ac474d
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-11-17 15:44:59 +0900

    feat: backjoon String Python_3029 경고

---
+++ Backjoon_python/Python_3029.py
@@ -0,0 +1,10 @@
+#경고
+h1,m1,s1 = map(int, input().split(":"))
+h2,m2,s2 = map(int, input().split(":"))
+t1 = h1*60*60+m1*60+s1
+t2 = h2*60*60+m2*60+s2
+t = t2-t1 if t2>t1 else t2-t1+24*60*60
+h=t//60//60
+m=t//60%60
+s=t%60
+print(f"{h:0>2}:{m:0>2}:{s:0>2}")
