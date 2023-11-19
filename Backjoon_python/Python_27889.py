717c96dba3421d3b2597bb690c40cd08be888a23
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-11-20 06:29:14 +0900

    feat: backjoon String Python_27889 특별한 학교 이름

---
+++ Backjoon_python/Python_27889.py
@@ -0,0 +1,10 @@
+#특별한 학교 이름
+msg = input()
+if msg == "NLCS":
+    print("North London Collegiate School")
+elif msg == "BHA":
+    print("Branksome Hall Asia")
+elif msg == "KIS":
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
