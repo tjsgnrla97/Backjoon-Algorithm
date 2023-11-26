ec4e9cf3a8f62c3b1dcb0b3cf01d93a6dc9d3955
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2023-11-26 18:28:07 +0900

    feat: backjoon String Python_5656 비교 연산자

---
+++ Backjoon_python/Python_5656.py
@@ -0,0 +1,34 @@
+#비교 연산자
+def compare_operator(exp):
+    answer = False
+    exp_split = exp.split()
+    
+    num1, operator, num2 = int(exp_split[0]), exp_split[1], int(exp_split[2])
+    
+    if operator == "E":
+        answer = -1
+    elif operator == "!=":
+        answer = (num1 != num2)
+    elif operator == "<":
+        answer = (num1 < num2)
+    elif operator == ">":
+        answer = (num1 > num2)
+    elif operator == "<=":
+        answer = (num1 <= num2)
+    elif operator == ">=":
+        answer = (num1 >= num2)
+    elif operator == "==":
+        answer = (num1 == num2)
+        
+    return answer
+
+case_num = 1
+while True:
+    exp = input()
+    result = compare_operator(exp)
+    
+    if result == -1:
+        break
+    else:
+        print(f"Case {case_num}: {str(result).lower()}")
+        case_num += 1
