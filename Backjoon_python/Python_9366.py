0a2d693c2623ae2a7e92b28ff91058234a163dc9
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-01-12 11:52:18 +0900

    feat: backjoon Math Python_9366 삼각형 분류

---
+++ Backjoon_python/Python_9366.py
@@ -0,0 +1,12 @@
+#삼각형 분류
+for tc in range(int(input())):
+    li = sorted(map(int,input().split()))
+    print(f"Case #{tc+1}: ", end='')
+    if li[0]+li[1] <= li[2]:
+        print("invalid!")
+    elif li[0] == li[1] == li[2]:
+        print("equilateral")
+    elif li[0]==li[1] or li[1]==li[2] or li[2]==li[0]:
+        print("isosceles")
+    else:
+        print("scalene")
