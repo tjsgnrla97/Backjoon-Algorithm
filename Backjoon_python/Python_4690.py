10155a36d9d56c1cbbd4f6daecae5929fe040b59
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-02-05 06:07:07 +0900

    feat: backjoon Math Python_4690 완전 세제곱

---
+++ Backjoon_python/Python_4690.py
@@ -0,0 +1,9 @@
+#완전 세제곱
+for a in range(2,101):
+    for b in range(2,101):
+        for c in range(b+1,101):
+            for d in range(c+1,101):
+                if a*a*a == (b*b*b+c*c*c+d*d*d):
+                    print(f"Cube = {a}, Triple = ({b},{c},{d})")
+                if a*a*a<(b*b*b+c*c*c+d*d*d):
+                    break
