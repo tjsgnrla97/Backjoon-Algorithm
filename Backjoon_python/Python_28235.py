8546175f29fac52c7ca4423ed38364643895128c
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-03-11 11:56:40 +0900

    feat: backjoon Implement Python_28235 코드마스터 2023

---
+++ Backjoon_python/Python_28235.py
@@ -0,0 +1,10 @@
+#코드마스터 2023
+chant = input()
+if chant == "SONGDO":
+    print("HIGHSCHOOL")
+elif chant == "CODE":
+    print("MASTER")
+elif chant == "2023":
+    print("0611")
+elif chant == "ALGORITHM":
+    print("CONTEST")
