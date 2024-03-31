c244de36d70f8982f6eca3720a5678b1120113cd
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-04-01 07:46:19 +0900

    feat: backjoon Implement Python_1871 좋은 자동차 번호판

---
+++ Backjoon_python/Python_1871.py
@@ -0,0 +1,8 @@
+#좋은 자동차 번호판
+for _ in range(int(input())):
+    L,D = input().split('-')
+    n = int(D)
+    s = 0
+    for i in range(3):
+        s += (ord(L[i])-65) * 26**(2-i)
+    print("nice" if abs(s-n) <= 100 else "not nice")
