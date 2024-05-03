69dc01d59e12300b61f46ab5cfe5b60a6bd03d8a
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-05-04 06:45:02 +0900

    feat: backjoon Implement Python_9501 꿍의 우주여행

---
+++ Backjoon_python/Python_9501.py
@@ -0,0 +1,9 @@
+#꿍의 우주여행
+for _ in range(int(input())):
+    n, d = map(int, input().split())
+    ans = 0
+    for i in range(n):
+        v, f, c = map(int,input().split())
+        if v * f/c >= d:
+            ans += 1
+    print(ans)
