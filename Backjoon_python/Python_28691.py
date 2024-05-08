fd9f93de0001c31da5074a08f50ae7fc988e3e1f
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-05-09 07:48:13 +0900

    feat: backjoon Implement Python_28691 정보보호학부 동아리 소개

---
+++ Backjoon_python/Python_28691.py
@@ -0,0 +1,12 @@
+#정보보호학부 동아리 소개
+msg = input()
+if msg == "M":
+    print("MatKor")
+elif msg == "W":
+    print("WiCys")
+elif msg == "C":
+    print("CyKor")
+elif msg == "A":
+    print("AlKor")
+else:
+    print("$clear")
