59a017963fd70cba9bffdee41a3d5b0c71a16045
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-05-23 10:57:59 +0900

    feat: backjoon Implement Python_13304 방 배정

---
+++ Backjoon_python/Python_13304.py
@@ -0,0 +1,21 @@
+#방 배정
+import math
+n,k = map(int,input().split())
+student = [0,0,0,0,0]
+
+for _ in range(n):
+    s,y = map(int,input().split())
+    if y==1 or y==2:
+        student[0] += 1
+    elif s==0 and (y==3 or y==4):
+        student[1] += 1
+    elif s==1 and (y==3 or y==4):
+        student[2] += 1
+    elif s==0 and (y==5 or y==6):
+        student[3] += 1
+    else:
+        student[4] += 1
+ans = 0
+for i in student:
+    ans += math.ceil(i/k)
+print(ans)
