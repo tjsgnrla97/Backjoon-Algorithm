637d606f574f6ef44ce8b44275283ecf583f15f2
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-05-30 16:30:51 +0900

    feat: backjoon Implement Python_15792 A/B-2

---
+++ Backjoon_python/Python_15792.py
@@ -0,0 +1,10 @@
+#A/B-2
+a,b = map(int,input().split())
+print(a//b,end='')
+if a%b:
+    print('.',end='')
+    i=0
+    while a%b and i<1000:
+        a = a%b * 10
+        i += 1
+        print(a//b, end='')
