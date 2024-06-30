817e93969f6e84df4fd747a26dd44c836af8a6c1
tjsgnrla97 <97587150+tjsgnrla97@users.noreply.github.com>
2024-07-01 06:17:46 +0900

    feat: backjoon Implement Python_25703 포인터 공부

---
+++ Backjoon_python/Python_25703.py
@@ -0,0 +1,11 @@
+#포인터 공부
+n = int(input())
+if n > 1:
+    print("int a;")
+    print("int *ptr = &a;")
+    print("int **ptr2 = &ptr;")
+    for i in range(3,n+1):
+        print("int "+"*"*i+"ptr"+str(i)+" = &ptr"+str(i-1)+";") #*
+else:
+    print("int a;")
+    print("int *ptr = &a;")
